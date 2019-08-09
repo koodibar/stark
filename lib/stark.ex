9b0c313895a97a703164988edba671d69483b02e
Hung Nguyen <hung.nguyen@digia.com>
2019-08-09 16:39:42 +0300

    Playing around

--- lib/stark.ex
+++ lib/stark.ex
@@ -15,4 +15,19 @@ defmodule Stark do
   def hello do
     :world
   end
+
+  def parse_markdown do
+    case File.read("posts/simple.md") do
+      {:ok, body} ->
+        {:ok, html_doc, _} = Earmark.as_html(body)
+        IO.puts(html_doc)
+        write_html(html_doc)
+      {:error, reason} ->
+        IO.puts(reason)
+    end
+  end
+
+  def write_html(body) do
+    File.write("outputs/simple.html", body)
+  end
 end
